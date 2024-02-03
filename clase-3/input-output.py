7d39b29c596369c871da7aa726e47c741a1d04cb
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-02-03 17:35:54 -0600

    feat: add class 3 contents

---
+++ clase-3/input-output.py
@@ -0,0 +1,13 @@
+# Salida
+# Print es para mostrar un mensaje
+# print("Hola")
+
+# Entrada
+# Input es para recibir un mensaje
+miNombre = input("Ingresa tu nombre: ")
+miNumeroFavorito = int(input("Ingresa tu número favorito: "))
+miOtroNumeroFavorito = int("14")
+
+# input siempre nos va a dar una cadena de texto
+print(miNumeroFavorito + 5)
+print(miNumeroFavorito)
