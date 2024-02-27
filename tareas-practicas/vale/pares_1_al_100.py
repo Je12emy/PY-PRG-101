330351f6c35b9e4156933f8b812dd5a89884a5a6
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-02-27 17:11:38 -0600

    feat: add practice excercises

---
+++ tareas-practicas/vale/pares_1_al_100.py
@@ -0,0 +1,10 @@
+# Numeros pares del 1 al 100
+a = 3
+b = 4
+
+if(a > b):
+ print(a*b)
+elif(b > a):
+ print(a+b) # 4 + 3 = 7
+else:
+ print("Fin")
---
+++ tareas-practicas/vale/tabla_de_2.py
@@ -0,0 +1,6 @@
+# Calcule los números pares del 1 al 100
+numero = 1
+while numero <= 100:
+     if numero % 2 == 0:
+        print(numero)
+     numero += 1
