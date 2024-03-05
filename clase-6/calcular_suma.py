796654d0a5ccfe03d3c6d69110a0c9e85589abe3
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-03-05 16:42:27 -0600

    feat: add materials

--- clase-6/calcular_suma.py
+++
@@ -1,9 +0,0 @@
-# Cree un programa llamado calculadora.py 
-# Pidanle al usuario que ingrese 2 numeros
-# Muestrenle al usuario el resultado de la sume de esos 2 numeros.
-# NOTA: use funciones
-
-def suma(a,b):
-    resultado = a + b
-    print(resultado)
-suma(1, 1)
